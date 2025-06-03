1652de06a9be46c81b3fbf1781362150771c6536
Dan Huss <dhuss@dhcp-10-24-123-44.dhcp.nd.edu>
2025-06-02 21:04:18 -0400

    securing api keys

--- web-dev/src/environments.js
+++ web-dev/src/environments.js
@@ -1,5 +1,5 @@
 export default {
-    APPLICATION_ID: "JYuuToLFr527wvq3Be7oWjWgA86bvvrIridk2kC6",
-    JAVASCRIPT_KEY: "4zyD4yvlZO0Mz3QJF0EJHvIqflZWxWA0OF5sfIaj",
+    APPLICATION_ID: import.meta.env.VITE_PARSE_APPLICATION_ID || "JYuuToLFr527wvq3Be7oWjWgA86bvvrIridk2kC6",
+    JAVASCRIPT_KEY: import.meta.env.VITE_PARSE_JAVASCRIPT_KEY || "4zyD4yvlZO0Mz3QJF0EJHvIqflZWxWA0OF5sfIaj",
     SERVER_URL: "https://parseapi.back4app.com"
   };
